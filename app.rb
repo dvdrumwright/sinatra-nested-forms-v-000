ac46790b50382efacab266d355c17363ecf6bbec
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-09-30 18:12:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ module FormsLab
         binding.pry
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ship].each do |details|
+      params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
       @ships = Ship.all
